5d2623b62d1e5ac8c0ab64dba2f47b54009d8195
BinaryAura <jadunker@hotmail.com>
2017-02-11 23:38:08 -0600

    1I0-0

---
+++ net/binaryaura/fmh/ForgeModHandler.cpp
@@ -0,0 +1,22 @@
+/*
+ * Forge Mod Handler is a terminal program that downloads, sets up,
+ * and monitors Forge mods.
+ *
+ * 11 February 2017
+ * Author: BinaryAura
+ *
+ * Version: 1I0-0
+ */
+
+#include <ncurses.h>
+
+int main() {
+  initscr();
+  printw("Hello World!");
+  getch();
+  endwin();
+
+  return 0;
+}
+
+
