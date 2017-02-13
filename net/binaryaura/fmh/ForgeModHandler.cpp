9cfa8db27e18a7ae7fcaea844814b2ebaa03a837
BinaryAura <jadunker@hotmail.com>
2017-02-13 15:34:48 -0600

    Mouse Tutorial
    http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/mouse.html
    
    Note: BUTTON1_PRESSED is holding the button, BUTTON1_CLICKED is click and release

--- net/binaryaura/fmh/ForgeModHandler.cpp
+++ net/binaryaura/fmh/ForgeModHandler.cpp
@@ -17,93 +17,104 @@ using namespace std;
 int startx = 0;
 int starty = 0;
 
-char *choices[] =
-  { "Create", "Import", "Manage", "Options", "Quit" };
+const char *choices[] = {"Create", "Import", "Manage", "Options", "Quit" };
 
 int n_choices = sizeof(choices) / sizeof(char *);
-void
-print_menu (WINDOW *menu_win, int highlight);
+void print_menu (WINDOW *menu_win, int highlight);
+void report_choice(int mouse_x, int mouse_y, int *p_choice);
 
-int
-main ()
+int main()
 {
+  int c, choice = 0;
   WINDOW *menu_win;
-  int highlight = 1;
-  int choice = 0;
-  int c;
+  MEVENT event;
 
-  initscr ();
-  clear ();
-  noecho ();
-  cbreak ();
+  /* Initialize curses */
+  initscr();
+  clear();
+  noecho();
+  cbreak(); //Line buffering disabled. pass on everything
+
+  /* Try to put the window in the middle of screen */
   startx = (80 - WIDTH) / 2;
   starty = (24 - HEIGHT) / 2;
 
-  menu_win = newwin (HEIGHT, WIDTH, starty, startx);
-  keypad (menu_win, true);
-  mvprintw (0, 0,
-	    "Use arrow keys to go up and down, Press enter to select a choice");
-  refresh ();
-  print_menu (menu_win, highlight);
-  while (true)
+  attron(A_REVERSE);
+  mvprintw(23, 1, "Click on Exit to quit (Works best in a virtual console)");
+  refresh();
+  attroff(A_REVERSE);
+
+  /* Print the menu for the first time */
+  menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+  print_menu(menu_win, 1);
+  keypad(menu_win, true);
+  /* Get all the mouse events */
+  mousemask(ALL_MOUSE_EVENTS, NULL);
+
+  while(true)
+  {
+    c = wgetch(menu_win);
+    switch(c)
     {
-      c = wgetch (menu_win);
-      switch (c)
-	{
-	case KEY_UP:
-	  if (highlight == 1)
-	    highlight = n_choices;
-	  else
-	    --highlight;
-	  break;
-	case KEY_DOWN:
-	  if (highlight == n_choices)
-	    highlight = 1;
-	  else
-	    ++highlight;
-	  break;
-	case 10:
-	  choice = highlight;
-	  break;
-	default:
-	  mvprintw (
-	      24, 0,
-	      "Character press is = %3d Hopefully it can be printed a '%c'", c,
-	      c);
-	  refresh ();
-	  break;
-	}
-      print_menu (menu_win, highlight);
-      if (choice != 0)
-	break;
+      case KEY_MOUSE:
+        if(getmouse(&event) == OK)
+        {
+          /* When the user clicks left mouse button */
+          if(event.bstate & BUTTON1_CLICKED)
+          {
+            report_choice(event.x + 1, event.y + 1, &choice);
+            if(choice == -1) //Exit chosen
+              goto end;
+            mvprintw(22, 1, "You chose %-10s", choices[choice]);
+            refresh();
+          }
+        }
+        print_menu(menu_win, choice);
+        break;
     }
-  mvprintw (23, 0, "You chose %s\n", choices[choice - 1]);
-  clrtoeol();
-  refresh ();
-  getch();
-  endwin ();
-  return 0;
+  }
+  end:
+    endwin();
+    return 0;
 }
 
-void
-print_menu (WINDOW *menu_win, int highlight)
+void print_menu(WINDOW *menu_win, int highlight)
 {
   int x, y, i;
 
   x = 2;
   y = 2;
-  box (menu_win, 0, 0);
-  for (i = 0; i < n_choices; ++i)
+  box(menu_win, 0, 0);
+  for(i = 0; i < n_choices; ++i)
+  {
+    if(highlight == i + 1)
     {
-      if (highlight == i + 1)
-	{
-	  wattron(menu_win, A_REVERSE);
-	  mvwprintw (menu_win, y, x, "%s", choices[i]);
-	  wattroff(menu_win, A_REVERSE);
-	}
-      else
-	mvwprintw (menu_win, y, x, "%s", choices[i]);
-      ++y;
+      wattron(menu_win, A_REVERSE);
+      mvwprintw(menu_win, y, x, "%s", choices[i]);
+      wattroff(menu_win, A_REVERSE);
     }
-  wrefresh (menu_win);
+    else
+      mvwprintw(menu_win, y, x, "%s", choices[i]);
+    ++y;
+  }
+  wrefresh(menu_win);
+}
+
+/* Report the choice according to mouse position */
+void report_choice(int mouse_x, int mouse_y, int *p_choice)
+{
+  int i,j, choice;
+
+  i = startx + 2;
+  j = starty + 3;
+
+  for(choice = 0; choice < n_choices; ++choice)
+    if(mouse_y == j + choice && mouse_x >= i && mouse_x <= i + (signed int)strlen(choices[choice]))
+    {
+      if(choice == n_choices - 1)
+        *p_choice = -1;
+      else
+        *p_choice = choice + 1;
+      break;
+    }
 }
